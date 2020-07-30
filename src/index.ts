f5e99a2e8a4d886b55ed9fbd4342ea5a3ce4e602
Marcel Kooi <1marcelkooi@gmail.com>
2020-07-29 22:30:54 -0400

    Add contrived example

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
-app.get('/', (req: any, res: any) => {
-  res.send('Hello World!');
-});
+const addTen = (input?: number): number | undefined => {
+  if (input) {
+    return input + 10;
+  }
+}
+
+app
+  .use(bodyParser.json())
+  .post('/', (req: Request, res: Response) => {
+    const { input } = req.body;
+    const newNum = addTen(input);
+    if (newNum) {
+      res.send(`The new number is: ${newNum}`);
+    } else {
+      res.send("New number could not be generated.")
+    }
+  });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
